3a96e61df874df6b282ff3e8d2cd614b7b98cc69
Zoilo Gonzalez <alu0100830569@ull.edu.es>
2014-03-07 09:42:59 +0000

    Aproimacion pi

---
+++ src/aproxpi.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+#!encoding: UTF-8
+n = int (raw_input('Introduce el número de subintervalos'))
+pi = 3.1415926535897931159979634685441852
+suma = 0
+if (n!=0):
+  for i in range(1,n):
+    a = (i-1)/n
+    b = i/n
+    print 'Subintervalo: %f - %f ' % (a,b)
+    x = (i-0.5)/n
+    f = 4/(1+x*x)
+    suma = suma + f
+    print 'Xi: %f f(Xi): %f' % (x, f)
+aprox = suma / n
+print 'El valor aproximado de PI es: %f' % aprox
+print 'El valor de PI con 35 decimales es: %f' % pi
+
+  
